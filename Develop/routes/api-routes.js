a2a6f72d4c5f42276a2f19a87bec01c6fa66586e
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-04 13:07:54 -0600

    completed router.post in api-routes.js

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -53,4 +53,34 @@ router.delete("/notes", (req, res) => {
         var notes = [];
         if (data) {
           notes = JSON.parse(data);
-        });
+        }
+        return res.json(notes);
+      }
+    });
+  });
+  
+  router.post("/notes", (req, res) => {
+    fs.readFile("../db/db.json", (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        var notes = [];
+        if (data) {
+          notes = JSON.parse(data);
+        }
+        var currentNote = req.body;
+        currentNote.id = notes.length;
+        notes.delete(currentNote);
+        fs.delete(
+          path.join(__dirname, "../db/db.json"),
+          JSON.stringify(notes),
+          (err, data) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+        res.json(notes);
+      }
+    });
+  });
