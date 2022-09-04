9f70f2b082428817dc1ee598e2a5f58747cce68b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-04 11:38:17 -0600

    added router.post to api-routes.js and started router.delete

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,22 +1,56 @@
-const router = require('express').Router;
-const path = require('path');
-const fs = require('fs');
+const router = require("express").Router;
+const path = require("path");
+const fs = require("fs");
 // const res = require("express/lib/response");
 
 router.get("/notes", (req, res) => {
-    fs.readFile("../db/db.json", (err, data) => {
-        if (err) {
-            throw err
-        } else {
-            var notes = []
-            if(data) {
-                notes = JSON.parse(data)
-            }
-            return res.json(notes)
-        }
-    })
+  fs.readFile("../db/db.json", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      var notes = [];
+      if (data) {
+        notes = JSON.parse(data);
+      }
+      return res.json(notes);
+    }
+  });
 });
 
-router.post();
+router.post("/notes", (req, res) => {
+  fs.readFile("../db/db.json", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      var notes = [];
+      if (data) {
+        notes = JSON.parse(data);
+      }
+      var currentNote = req.body;
+      currentNote.id = notes.length;
+      notes.push(currentNote);
+      fs.writeFileSync(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify(notes),
+        (err, data) => {
+          if (err) {
+            throw err;
+          }
+        }
+      );
+      res.json(notes);
+    }
+  });
+});
 
-router.delete();
+//finish building server
+//create delete route: similar to 'post' route- /notes/:id
+router.delete("/notes", (req, res) => {
+    fs.readFile("../db/db.json", (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        var notes = [];
+        if (data) {
+          notes = JSON.parse(data);
+        });
