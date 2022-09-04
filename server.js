1c29ad852a21bf26d7442b472b5410c05bee6a89
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-04 10:28:49 -0600

    some comments and listener app in server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+//dependancies
 const express = require('express');
 const fs = require("fs");
 const path = require('path');
 
+//express app
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+//listener app
+app.listen(PORT, () =>
+  console.log(`good copy, good buddy:${PORT}`)
+);
+
