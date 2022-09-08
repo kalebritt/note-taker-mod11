0b482e1228d2e8ab55aea7850fa9a4188099fb4f
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-08 08:04:32 -0600

    app.use in server.js: why is my clientRoute a method??

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 //dependancies and express app
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 //routes
 const apiRoute = require("./Develop/routes/api-routes");
 
-const clientRoute = require("./Develop/routes/client-routes")
+const clientRoute = require("./Develop/routes/client-routes");
 
 // boilerplate?
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-
+app.use("/api", apiRoute);
+app.use("/", clientRoute);
 
 //listener app
 app.listen(PORT, () =>
   console.log(`good copy, good buddy, at http://localhost:${PORT}`)
 );
-
